1ba620ed2e2afad136607d0eee0b6a40925b742a
BayanHammami <bayan.hammami@gmail.com>
2018-04-01 14:08:13 +1000

    Sparklyr data manipulation and ML

--- sparklyr.r
+++ sparklyr.r
@@ -1,1 +1,79 @@
-install.packages("sparklyr")
+#install.packages('sparklyr')
+#spark_install(version = "2.1.0")
+#install.packages(c("nycflights13", "Lahman"))
+library(sparklyr)
+library(dplyr)
+library(nycflights13)
+library(ggplot2)
+
+sc <- spark_connect(master = "local", version = "2.1.0")
+
+flights <- copy_to(sc, flights, "flights")
+airlines <- copy_to(sc, airlines, "airlines")
+
+select(flights, year:day, arr_delay, dep_delay)
+
+c1 <- filter(flights, day == 17, month == 5, carrier %in% c('UA', 'WN', 'AA', 'DL'))
+c2 <- select(c1, year, month, day, carrier, dep_delay, air_time, distance)
+c3 <- arrange(c2, year, month, day, carrier)
+c4 <- mutate(c3, air_time_hours = air_time / 60)
+
+c4 <- flights %>%
+  filter(month == 5, day == 17, carrier %in% c('UA', 'WN', 'AA', 'DL')) %>%
+  select(carrier, dep_delay, air_time, distance) %>%
+  arrange(carrier) %>%
+  mutate(air_time_hours = air_time / 60)
+
+c4 %>%
+  group_by(carrier) %>%
+  summarize(count = n(), mean_dep_delay = mean(dep_delay))
+
+data <- as.data.frame(c4)
+
+c4 <- collect(c4)
+
+data <- flights %>% left_join(airlines)
+data <- collect(data)
+
+spark_write_parquet(flights, "./data.parquet")
+
+iris_tbl <- copy_to(sc, iris, "iris", overwrite = TRUE)
+iris_tbl
+
+kmeans_model <- iris_tbl %>%
+  select(Petal_Width, Petal_Length) %>%
+  ml_kmeans(formula= ~ Petal_Width + Petal_Length, centers = 3)
+
+kmeans_model
+
+predicted <- sdf_predict(kmeans_model, iris_tbl) %>%
+  collect()
+
+View(predicted)
+
+sdf_predict(kmeans_model) %>%
+  collect() %>%
+  ggplot(aes(Petal_Length, Petal_Width)) +
+  geom_point(aes(Petal_Width, Petal_Length, col = factor(prediction + 1)),
+             size = 2, alpha = 0.5) + 
+  geom_point(data = kmeans_model$centers, aes(Petal_Width, Petal_Length),
+             col = scales::muted(c("red", "green", "blue")),
+             pch = 'x', size = 12) +
+  scale_color_discrete(name = "Predicted Cluster",
+                       labels = paste("Cluster", 1:3)) +
+  labs(
+    x = "Petal Length",
+    y = "Petal Width",
+    title = "K-Means Clustering",
+    subtitle = "Use Spark.ML to predict cluster membership with the iris dataset."
+  )
+
+
+#Random forest
+rf_model <- iris_tbl %>%
+  ml_random_forest(Species ~ Petal_Length + Petal_Width, type = "classification")
+rf_predict <- sdf_predict(rf_model, iris_tbl) %>%
+  ft_string_indexer("Species", "Species_idx") %>%
+  collect
+
+table(rf_predict$Species_idx, rf_predict$prediction)
