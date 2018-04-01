6315a08a2259b17cdb0f08e3135952951a6a87a4
BayanHammami <bayan.hammami@gmail.com>
2018-04-01 14:24:04 +1000

    mlworkflow branch

--- sparklyr.r
+++ sparklyr.r
@@ -8,72 +8,30 @@ library(ggplot2)
 
 sc <- spark_connect(master = "local", version = "2.1.0")
 
-flights <- copy_to(sc, flights, "flights")
-airlines <- copy_to(sc, airlines, "airlines")
+mtcars_tbl <- copy_to(sc, mtcars, "mtcars")
+partitions <- mtcars_tbl %>%
+  filter(hp >= 100) %>%
+  sdf_mutate(cyl8 = ft_bucketizer(cyl, c(0,8,12))) %>%
+  sdf_partition(training = 0.5, test = 0.5, seed = 888)
 
-select(flights, year:day, arr_delay, dep_delay)
+# fit a linear mdoel to the training dataset
+fit <- partitions$training %>%
+  ml_linear_regression(mpg ~ wt + cyl)
 
-c1 <- filter(flights, day == 17, month == 5, carrier %in% c('UA', 'WN', 'AA', 'DL'))
-c2 <- select(c1, year, month, day, carrier, dep_delay, air_time, distance)
-c3 <- arrange(c2, year, month, day, carrier)
-c4 <- mutate(c3, air_time_hours = air_time / 60)
+summary(fit)
 
-c4 <- flights %>%
-  filter(month == 5, day == 17, carrier %in% c('UA', 'WN', 'AA', 'DL')) %>%
-  select(carrier, dep_delay, air_time, distance) %>%
-  arrange(carrier) %>%
-  mutate(air_time_hours = air_time / 60)
+pred <- sdf_predict(fit, partitions$test) %>%
+  collect
 
-c4 %>%
-  group_by(carrier) %>%
-  summarize(count = n(), mean_dep_delay = mean(dep_delay))
-
-data <- as.data.frame(c4)
-
-c4 <- collect(c4)
-
-data <- flights %>% left_join(airlines)
-data <- collect(data)
-
-spark_write_parquet(flights, "./data.parquet")
-
-iris_tbl <- copy_to(sc, iris, "iris", overwrite = TRUE)
-iris_tbl
-
-kmeans_model <- iris_tbl %>%
-  select(Petal_Width, Petal_Length) %>%
-  ml_kmeans(formula= ~ Petal_Width + Petal_Length, centers = 3)
-
-kmeans_model
-
-predicted <- sdf_predict(kmeans_model, iris_tbl) %>%
-  collect()
-
-View(predicted)
-
-sdf_predict(kmeans_model) %>%
-  collect() %>%
-  ggplot(aes(Petal_Length, Petal_Width)) +
-  geom_point(aes(Petal_Width, Petal_Length, col = factor(prediction + 1)),
-             size = 2, alpha = 0.5) + 
-  geom_point(data = kmeans_model$centers, aes(Petal_Width, Petal_Length),
-             col = scales::muted(c("red", "green", "blue")),
-             pch = 'x', size = 12) +
-  scale_color_discrete(name = "Predicted Cluster",
-                       labels = paste("Cluster", 1:3)) +
+ggplot(pred, aes(x = mpg, y = prediction)) +
+  geom_abline(lty = "dashed", col = "red") +
+  geom_point() +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  coord_fixed(ratio = 1) +
   labs(
-    x = "Petal Length",
-    y = "Petal Width",
-    title = "K-Means Clustering",
-    subtitle = "Use Spark.ML to predict cluster membership with the iris dataset."
+    x = "Actual Fuel Consumption",
+    y = "Predicted Fuel Consumption",
+    title = "Predicted vs. Actual Fuel Consumption"
   )
 
 
-#Random forest
-rf_model <- iris_tbl %>%
-  ml_random_forest(Species ~ Petal_Length + Petal_Width, type = "classification")
-rf_predict <- sdf_predict(rf_model, iris_tbl) %>%
-  ft_string_indexer("Species", "Species_idx") %>%
-  collect
-
-table(rf_predict$Species_idx, rf_predict$prediction)
